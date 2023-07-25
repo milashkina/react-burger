4d496abc97e93e2b0c6e65cd00470a2a7a7faa7b
milashkina <milakina.alena.developer@gmail.com>
2023-07-25 20:53:04 +0300

    JSX main pages

---
+++ src/components/burger-ingredients/BurgerIngredients.js
@@ -0,0 +1,33 @@
+import {Tabs} from './Tab'
+import React from "react";
+import style from '../burger-ingredients/burgerIngredients.module.css'
+import ProductList from "../product-list/product-list";
+import {data} from "../../utils/data";
+
+export default function BurgerIngredients() {
+
+
+  return(
+    <div className={``}>
+      <span className={`text text_type_main-large pb-5`}> Соберите бургер</span>
+      <Tabs />
+      <div className={`${style.containerIngredients}`}>
+        <span className={`text text_type_main-medium`}>Булки</span>
+        <div className={`${style.wrapperCard} pt-6 pb-10 pl-4`}>
+          <ProductList props={data} />
+          <ProductList props={data} />
+        </div>
+        <span className={`text text_type_main-medium`}>Соусы</span>
+        <div className={`${style.wrapperCard} pt-6 pb-10 pl-4`}>
+          <ProductList props={data} />
+          <ProductList props={data} />
+        </div>
+        <span className={`text text_type_main-medium`}>Начинки</span>
+        <div className={`${style.wrapperCard} pt-6 pb-10 pl-4`}>
+          <ProductList props={data} />
+          <ProductList props={data} />
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/product-list/product-list.js
@@ -0,0 +1,17 @@
+import {CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import style from '../product-list/product-list.module.css'
+
+export default function ProductList(props) {
+  const {name, price, image} = props;
+  return(
+    <div className={`${style.wrapperCard}`}>
+      <img src='https://code.s3.yandex.net/react/code/bun-02.png' alt={name} className={`p-4`} />
+      <div className={`${style.wrapper_currency} + pt-1 pb-1 `}>
+        <span className={`text text_type_digits-default`}>1255</span>
+        <CurrencyIcon type="primary" />
+      </div>
+      <span className={`text text_type_main-default`}>Краторная булка N-200i</span>
+    </div>
+
+  )
+}
