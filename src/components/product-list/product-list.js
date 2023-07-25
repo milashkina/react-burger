6a04f59e29b9eb2e8ebd36fc9067517fe64bf978
milashkina <milakina.alena.developer@gmail.com>
2023-07-25 20:57:05 +0300

    add JSX for main page

--- src/components/product-list/product-list.js
+++ src/components/product-list/product-list.js
@@ -1,10 +1,11 @@
-import {CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import {Counter, CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import style from '../product-list/product-list.module.css'
 
 export default function ProductList(props) {
   const {name, price, image} = props;
   return(
     <div className={`${style.wrapperCard}`}>
+      <Counter count={1} size="default" extraClass="m-1" />
       <img src='https://code.s3.yandex.net/react/code/bun-02.png' alt={name} className={`p-4`} />
       <div className={`${style.wrapper_currency} + pt-1 pb-1 `}>
         <span className={`text text_type_digits-default`}>1255</span>
