855ca18316461cb791e0db0bf1161f9a2d564193
milashkina <milakina.alena.developer@gmail.com>
2023-07-25 20:53:04 +0300

    add JSX for main page
    JSX main pages

---
+++ src/components/app-header/AppHeader.js
@@ -0,0 +1,26 @@
+import {BurgerIcon, ListIcon, Logo, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import style from "../app-header/appHeader.module.css"
+export default function AppHeader() {
+
+  return(
+    <header className={`${style.appHeader} + p-4`}>
+      <div className={`${style.navMenu}`}>
+        <div className={`${style.container} + p-4 text text_type_main-default`}>
+          <div className={`${style.container} + mr-2`} ><BurgerIcon type="primary" /></div>
+          Конструктор
+        </div>
+        <div className={`${style.container} + p-2 text text_type_main-default text_color_inactive`}>
+          <div className={`${style.container} + mr-2`} ><ListIcon type="secondary" /></div>
+          Лента заказов
+        </div>
+      </div>
+      <div>
+        <Logo />
+      </div>
+      <div className={`${style.container} + p-2 text text_type_main-default text_color_inactive`}>
+        <div className={`${style.container} + mr-2`} ><ProfileIcon type="secondary" /></div>
+        Личный кабинет
+      </div>
+    </header>
+  )
+}
