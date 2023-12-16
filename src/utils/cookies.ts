14d9fc6c5977aa3bf21f7e783d3983e2509a1bd4
milashkina <milakina.alena.developer@gmail.com>
2023-12-16 10:21:29 +0300

    hotfix strong typed setCookie

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,34 +1,50 @@
-
-// TODO: понять на какой тип поменять props any
-export const setCookie = (name: string, value: string | number | boolean, props: any) => {
-  props = props || {};
-  let exp = props.expires;
-  if (typeof exp == 'number' && exp) {
-    const d = new Date();
-    d.setTime(d.getTime() + exp * 1000);
-    exp = props.expires = d;
-  }
-  if (exp && exp.toUTCString) {
-    props.expires = exp.toUTCString();
-  }
-  value = encodeURIComponent(value);
-  let updatedCookie = name + '=' + value;
-  for (const propName in props) {
-    updatedCookie += '; ' + propName.toString();
-    const propValue = props[propName];
-    if (propValue !== true) {
-      updatedCookie += '=' + propValue;
-    }
-  }
-  document.cookie = updatedCookie;
-}
 export function getCookie(name: string) {
   const matches = document.cookie.match(
-    new RegExp('(?:^|; )' + name.replace(/([.$?*|{}()[\]\\/+^])/g, '\\$1') + '=([^;]*)')
+      new RegExp('(?:^|; )' + name.replace(/([.$?*|{}()[\]\\/+^])/g, '\\$1') + '=([^;]*)')
   );
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
-export function deleteCookie(name: string) {
-  setCookie(name, '', { expires: -1 });
+interface CookieProps {
+  path?: string;
+  expires?: string | number | Date;
+  [key: string]: any;
+}
+
+export function setCookie(name: string, value: string, props: CookieProps = {}): void {
+  const defaultProps: CookieProps = {
+    path: "/",
+    SameSite: "Strict"
+  };
+
+  const mergedProps = { ...defaultProps, ...props };
+
+  let { expires } = mergedProps;
+
+  if (typeof expires === "number") {
+    const d = new Date();
+    d.setTime(d.getTime() + expires * 1000);
+    expires = mergedProps.expires = d;
+  }
+
+  if (expires && expires.toString) {
+    mergedProps.expires = expires.toString();
+  }
+
+  const propStrings = Object.entries(mergedProps).map(([key, value]) => {
+    if (value === true) {
+      return key;
+    }
+    return `${key}=${value}`;
+  });
+
+  const propString = propStrings.join("; ");
+
+  const encodedValue = encodeURIComponent(value);
+
+  document.cookie = `${name}=${encodedValue}; ${propString}`;
+}
+
+export function deleteCookie(name: string): void {
+  setCookie(name, "", { "max-age": -1 });
 }
